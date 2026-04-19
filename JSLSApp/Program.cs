0ca14c0a393b7ea9471dcc9dd3a96218ea73b9ec
Luthetus <luthetus@gmail.com>
2026-04-19 16:10:16 -0400

    types

--- JSLSApp/Program.cs
+++ JSLSApp/Program.cs
@@ -476,5 +476,134 @@ class TextDocumentItem
     public string? text { get; set; }
 }
 
+class DocumentSymbol
+{
+    /**
+	 * The name of this symbol. Will be displayed in the user interface and
+	 * therefore must not be an empty string or a string only consisting of
+	 * white spaces.
+	 */
+    public string name { get; set; }
 
+    /**
+	 * More detail for this symbol, e.g the signature of a function.
+	 */
+    public string detail { get; set; }
 
+    /**
+	 * The kind of this symbol.
+	 */
+    public SymbolKind kind { get; set; }
+
+    /**
+	 * Tags for this document symbol.
+	 *
+	 * @since 3.16.0
+	 */
+    public SymbolTag[]? tags { get; set; }
+
+    /**
+	 * Indicates if this symbol is deprecated.
+	 *
+	 * @deprecated Use tags instead
+	 */
+    public bool deprecated { get; set; }
+
+    /**
+	 * The range enclosing this symbol not including leading/trailing whitespace
+	 * but everything else like comments. This information is typically used to
+	 * determine if the clients cursor is inside the symbol to reveal it  in the
+	 * UI.
+	 */
+    public Range range { get; set; }
+
+    /**
+	 * The range that should be selected and revealed when this symbol is being
+	 * picked, e.g. the name of a function. Must be contained by the `range`.
+	 */
+    public Range selectionRange { get; set; }
+
+    /**
+	 * Children of this symbol, e.g. properties of a class.
+	 */
+    public DocumentSymbol[] children { get; set; }
+}
+
+enum SymbolKind
+{
+    File = 1,
+    Module = 2,
+    Namespace = 3,
+    Package = 4,
+    Class = 5,
+    Method = 6,
+    Property = 7,
+    Field = 8,
+    Constructor = 9,
+    Enum = 10,
+    Interface = 11,
+    Function = 12,
+    Variable = 13,
+    Constant = 14,
+    String = 15,
+    Number = 16,
+    Boolean = 17,
+    Array = 18,
+    Object = 19,
+    Key = 20,
+    Null = 21,
+    EnumMember = 22,
+    Struct = 23,
+    Event = 24,
+    Operator = 25,
+    TypeParameter = 26,
+}
+
+/**
+ * Symbol tags are extra annotations that tweak the rendering of a symbol.
+ *
+ * @since 3.16
+ */
+enum SymbolTag
+{
+    /**
+	 * Render a symbol as obsolete, usually using a strike-out.
+	 */
+    Deprecated = 1,
+}
+
+class Range
+{
+    /**
+	 * The range's start position.
+	 */
+    public Position start { get; set; }
+
+    /**
+	 * The range's end position.
+	 */
+    public Position end { get; set; }
+}
+
+class Position
+{
+    /**
+	 * Line position in a document (zero-based).
+	 * 
+	 * This is a comment from myself not the docs:
+	 *     TODO: consider uint because the docs specifically said 'uinteger'
+	 */
+    public int line { get; set; }
+
+    /**
+	 * Character offset on a line in a document (zero-based). The meaning of this
+	 * offset is determined by the negotiated `PositionEncodingKind`.
+	 *
+	 * If the character value is greater than the line length it defaults back
+	 * to the line length.
+	 * 
+	 * This is a comment from myself not the docs:
+	 *     TODO: consider uint because the docs specifically said 'uinteger'
+	 */
+    public int character { get; set; }
+}

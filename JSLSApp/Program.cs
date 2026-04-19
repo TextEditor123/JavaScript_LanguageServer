ae197866de2461dc988b2cef1714ca1503d4ed06
Luthetus <luthetus@gmail.com>
2026-04-19 08:03:23 -0400

    await reader.ReadLineAsync();

--- JSLSApp/Program.cs
+++ JSLSApp/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using (StreamReader reader = new StreamReader(Console.OpenStandardInput()))
+{
+    // I'm getting the warning: "Do not use 'reader.EndOfStream' in an async method"
+    while (!reader.EndOfStream)
+    {
+        string line = await reader.ReadLineAsync();
+        // Process line
+    }
+}

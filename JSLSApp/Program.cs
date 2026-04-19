a3aa51a8e188cd8c93480b51a23fd3ccd6664671
Luthetus <luthetus@gmail.com>
2026-04-19 08:48:42 -0400

    Update Program.cs

--- JSLSApp/Program.cs
+++ JSLSApp/Program.cs
@@ -1,13 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-/*using (StreamReader reader = new StreamReader(Console.OpenStandardInput()))
+string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+if (homePath != "C:\\Users\\hunte")
 {
-    // I'm getting the warning: "Do not use 'reader.EndOfStream' in an async method"
-    while (!reader.EndOfStream)
-    {
-        string line = await reader.ReadLineAsync();
-        // Process line
-    }
-}*/
+    Console.WriteLine(@"if (homePath != ""C:\\Users\\hunte"")");
+    return;
+}
+
+//using StreamWriter writer = new StreamWriter("C:\\Users\\hunte\\Repos\\file.txt");
+var myPath = "C:\\Users\\hunte\\Repos\\file.txt";
+File.WriteAllText(myPath, string.Empty);
+
+using StreamReader reader = new StreamReader(Console.OpenStandardInput());
+
+while (!reader.EndOfStream) // I'm getting the warning: "Do not use 'reader.EndOfStream' in an async method"
+{
+    string line = await reader.ReadLineAsync();
+    File.AppendAllText(myPath, line);
+    File.AppendAllText(myPath, "\n====\n");
+    // Process line
+}
